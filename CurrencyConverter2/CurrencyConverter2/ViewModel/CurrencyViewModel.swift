ffa4409ab312e28e7d417d2200a6e783b3b0513e
SharonB9 <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>
2022-02-07 00:26:28 +0100

    ViewModel setUp

---
+++ CurrencyConverter2/CurrencyConverter2/ViewModel/CurrencyViewModel.swift
@@ -0,0 +1,66 @@
+import Foundation
+import RealmSwift
+
+class CurrencyViewModel {
+    
+    
+    var timeStamp = 0
+    var date = ""
+    var loadCurrencyDropDown: [String] = []
+   
+    var base: [String] = [String]()
+    var urlString: String?
+   
+    var readDataSaved : ((Bool) -> Void)?
+    var rateArray = [Double]()
+    var getConversionRate: (([Double]) -> Void)?
+    var getDate: ((Int) -> Void)?
+    
+    init(apiString: String){
+        self.urlString = apiString
+        apiClass = ApiCall(urlLink: urlString ?? "maximum number of calls have been made please change Apikey")
+    }
+    
+    func saveData(){
+        apiClass?.getData(completionHandler: { [self] result in
+            self.base.append(result.base)
+            self.date = result.date
+            self.timeStamp = result.timestamp
+            
+            for (key, value) in result.rates{
+                persistRate = CurrencyDictionary()
+                persistRate?.currency = key
+                persistRate?.rate = value
+                currency.currency.append(persistRate!)
+                rateArray.append(value)
+                getDate?(timeStamp)
+                getConversionRate?(rateArray)
+            }
+            
+            persistRealm.delete()
+            persistRealm.saveData(of: currency)
+            readDataSaved?(true)
+        })
+    }
+    
+    func passRetreivedData(completionHandler: ( @escaping (Container) -> Void )){
+        var a : Container?
+        readDataSaved = { dataAvailable in
+            if dataAvailable {
+                self.fetchDataFromRealm()
+                a = Container(base: self.base, currency: self.loadCurrencyDropDown)
+                completionHandler(a!)
+            }
+        }
+    }
+    
+    func fetchDataFromRealm(){
+        self.persistedRateList = persistRealm.readData()
+        let data = persistedRateList?.first?.currency
+        if let keys = data {
+            for i in 0..<keys.count {
+                loadCurrencyDropDown.append(keys[i].currency)
+            }
+        }
+    }
+}
