7319fc1cb948b451238cc7d702f99ba6b6b7025b
SharonB9 <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>
2022-02-07 00:45:19 +0100

    ui updated

--- CurrencyConverter2/CurrencyConverter2/View/Controller/CurrencyConverterController.swift
+++ CurrencyConverter2/CurrencyConverter2/View/Controller/CurrencyConverterController.swift
@@ -1,1 +1,97 @@
-import Foundation
+import UIKit
+import Alamofire
+import DropDown
+import Charts
+
+class ViewController: UIViewController, ChartViewDelegate {
+//    var chartData = LineChartData(dataSets: [])
+  
+    var dataEntry = ChartDataEntry()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        configureViews()
+        convertedCurrencyTextField.isUserInteractionEnabled = false
+        let dismissKeyboard = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboardWhenTapped))
+        dismissKeyboard.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(dismissKeyboard)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        configureLayoutViews()
+        configureLineChartView()
+    }
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var firstDropDownView: UIView!
+    @IBOutlet weak var firstDropDownLabel: UILabel!
+    @IBAction func firstDropDownButton(_ sender: Any) {
+        firstCurrencyDropDown.show()
+    }
+    @IBOutlet weak var secondDropDownView: UIView!
+    @IBOutlet weak var secondDropDownLabel: UILabel!
+    @IBAction func secondDropDownButton(_ sender: Any) {
+        secondCurrencyDropDown.show()
+    }
+    @IBOutlet weak var lineChartView: LineChartView!
+    @IBOutlet weak var BottomUIView: UIView!
+    @IBOutlet weak var labelOfCurrencyToBeConverted: UILabel!
+    @IBOutlet weak var labelOfCurrencyDesired: UILabel!
+    @IBOutlet weak var thirtyDaysImage: UIImageView!
+    @IBOutlet weak var thirtyDaysButton: UIButton!
+    @IBOutlet weak var sixtyDaysImage: UIImageView!
+    @IBOutlet weak var sixtyDaysButton: UIButton!
+    @IBOutlet weak var textFieldOfAmountToBeConverted: UITextField!
+    @IBOutlet weak var convertedCurrencyTextField: UITextField!
+    @IBOutlet weak var currentTime: UILabel!
+    @IBAction func convertButton(_ sender: Any) {
+        guard let firstCurrencyText = textFieldOfAmountToBeConverted.text else {
+            return
+        }
+        guard let firstCurrencyValue = Double(firstCurrencyText) else {
+            return
+        }
+        convertedCurrencyTextField.text = "\(firstCurrencyValue * rateAtIndex)"
+    }
+    
+    @IBAction func past30DaysChartButton(_ sender: Any) {
+        sixtyDaysButton.alpha = 0.5
+        sixtyDaysImage.alpha = 0
+        thirtyDaysButton.alpha = 1
+        thirtyDaysImage.alpha = 1
+    }
+    
+    @IBAction func past60DaysChartButton(_ sender: Any) {
+        thirtyDaysButton.alpha = 0.5
+        thirtyDaysImage.alpha = 0
+        sixtyDaysButton.alpha = 1
+        sixtyDaysImage.alpha = 1
+    }
+    
+    @IBAction func currencyShuffleButton(_ sender: Any) {
+        let currencyShuffleTuple = (secondDropDownLabel.text, firstDropDownLabel.text)
+        firstDropDownLabel.text = currencyShuffleTuple.0
+        secondDropDownLabel.text = currencyShuffleTuple.1
+    }
+    
+    @objc func dismissKeyboardWhenTapped() {
+        self.view.endEditing(true)
+    }
+    
+    let searchController = UISearchController()
+    var currencyRates = [Double]()
+    var rateAtIndex = 0.0
+    let months = ["01 Jan", "07 Jun", "15 Jun", "23 Jun", "30 Jun"]
+    let unitsSold = [50.0, 25.0, 50.0, 75.0, 100.0, 75.0]
+    var dataEntries: [ChartDataEntry] = []
+    let viewModel = CurrencyViewModel(apiString: "http://data.fixer.io/api/latest?access_key=e495167586ff929ca03db8c2c900ff94&format=1")
+    lazy var firstCurrencyLogo = [String]()
+    lazy var secondCurrencyLogo = [String]()
+    let secondCurrencyDropDown = DropDown()
+    lazy var firstCurrencyDropDown = DropDown()
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+}
